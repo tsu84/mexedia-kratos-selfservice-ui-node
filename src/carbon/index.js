4be272f17e9440d99032c6eef31bf1b2ca1a7aeb
cosmin-z <cosmin.zaharia@hotmail.com>
2022-06-22 17:36:47 +0200

    mixin

---
+++ src/carbon/index.js
@@ -0,0 +1,15 @@
+import '../../node_modules/carbon-web-components/es/components/dropdown/dropdown.js';
+import '../../node_modules/carbon-web-components/es/components/dropdown/dropdown-item.js';
+import '../../node_modules/carbon-web-components/es/components/input';
+import '../../node_modules/carbon-web-components/es/components/button/button.js';
+import '../../node_modules/carbon-web-components/es/components/ui-shell/header.js';
+import '../../node_modules/carbon-web-components/es/components/ui-shell/header-nav.js';
+import '../../node_modules/carbon-web-components/es/components/ui-shell/header-nav-item.js';
+import '../../node_modules/carbon-web-components/es/components/ui-shell/header-menu.js';
+import '../../node_modules/carbon-web-components/es/components/ui-shell/header-menu-item.js';
+import '../../node_modules/carbon-web-components/es/components/ui-shell/header-menu-button.js';
+import '../../node_modules/carbon-web-components/es/components/ui-shell/header-name.js';
+import './index.scss';
+
+
+
---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const sass = require('sass');
+const autoprefixer = require('autoprefixer');
+
+module.exports = {
+    mode: 'development', // development/testing/staging/production
+    entry: './src/carbon/index.js', // The entry point where module bundler fetches file to be converted
+    output: {
+        path: path.resolve('public', 'dist'), // The absolute path of your dist folder
+        filename: 'bundle.js' // The filename of your final JS bundled file
+    },
+    watch: true,
+    module: {
+        rules: [
+        {
+            test: /\.scss$/,
+            use: [
+                {
+                    loader: 'style-loader',
+                },
+                {
+                    loader: 'css-loader',
+                    options: {
+                        importLoaders: 2,
+                        sourceMap: true,
+                    },
+                },
+                {
+                    loader: 'postcss-loader',
+                    options: {
+                        postcssOptions: {
+                            // `autoprefixer` is a requirement for carbon core Sass code
+                            plugins: [autoprefixer],
+                        },
+                    },
+                },
+                {
+                    loader: 'sass-loader',
+                    options: {
+                        implementation: sass,
+                        sassOptions: {
+                            includePaths: ['node_modules'],
+                            // `enable-css-custom-properties` and `grid-columns-16` feature flags
+                            // are requirements for carbon for IBM.com styles
+                            data: `
+                  $feature-flags: (
+                    enable-css-custom-properties: true,
+                    grid-columns-16: true,
+                  );
+                `,
+                        },
+                    },
+                },
+            ],
+        },
+    ],
+    },
+}
+
+
