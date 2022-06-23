fe4246fba7a6c8bd092dcedaf84954a38c2888f5
cosmin-z <cosmin.zaharia@hotmail.com>
2022-06-23 12:57:22 +0200

    fixed mobile

--- src/carbon/index.js
+++ src/carbon/index.js
@@ -9,6 +9,7 @@ import '../../node_modules/carbon-web-components/es/components/ui-shell/header-m
 import '../../node_modules/carbon-web-components/es/components/ui-shell/header-menu-item.js';
 import '../../node_modules/carbon-web-components/es/components/ui-shell/header-menu-button.js';
 import '../../node_modules/carbon-web-components/es/components/ui-shell/header-name.js';
+import '../../node_modules/carbon-web-components/es/components/checkbox/index.js';
 import './index.scss';
 
 
